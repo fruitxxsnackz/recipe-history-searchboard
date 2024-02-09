67810804d754142dd39bb444b49be4e44b828e25
fruitxxsnackz <Joanna1162494@gmail.com>
2024-02-08 19:05:27 -0800

    Uploading HTML/Java

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
 
+// Making sure functions run after HTML loads.
 document.addEventListener('DOMContentLoaded', function() {
-    var pagearray = [];
+  var pagearray = [];
+  // Run fetchwikipedia when button is clicked; API documentation.
+  document.getElementById('searchBtn').addEventListener('click', function () {
+      var searchinput = document.getElementById('searchBarInput').value.toLowerCase();
+      var api = 'https://en.wikipedia.org/w/api.php?action=query&format=json&titles='+searchinput+'&prop=extracts&exintro&origin=*';
 
-    document.getElementById('searchBtn').addEventListener('click', function () {
-        var searchinput = document.getElementById('searchBarInput').value.toLowerCase();
-        var api = 'https://en.wikipedia.org/w/api.php?action=query&format=json&titles='+searchinput+'&prop=extracts&exintro&origin=*';
+      fetchwikipedia(api);
+  });
 
-        fetchWikipedia(api);
-    });
+  //API fetch request; extract data and store in an array; calling showhistory function.
+  function fetchwikipedia(api) {
+      fetch(api).then(function(response) {
+          if (response.ok) {
+              response.json().then(function (data) {
+                  var infosections = data.query.pages;
+                  var headsec = Object.keys(infosections)[0];
+                  var extractpage = infosections[headsec].extract;
+                  pagearray.push(extractpage);
+                  showHistory(pagearray);
+              });
+          } else if (response.status !== 200) {
+              throw "Status Error";
+          }
+      });
+  }
 
-    function fetchWikipedia(api) {
-        fetch(api).then(function(response) {
-            if (response.ok) {
-                response.json().then(function (data) {
-                    var infosections = data.query.pages;
-                    var headsec = Object.keys(infosections)[0];
-                    var extractpage = infosections[headsec].extract;
-                    pagearray.push(extractpage);
-                    showHistory(pagearray);
-                });
-            } else if (response.status !== 200) {
-                throw "Status Error";
-            }
-        });
-    }
+  // Making sure there is no prev info; create element for retrieved history; append elements so it can show on HTML.
+  function showHistory(history) {
+      var WikiData = document.getElementById('data');
+      WikiData.textContent = '';
 
-    function showHistory(history) {
-        var WikiData = document.getElementById('data');
-        WikiData.textContent = '';
-
-        for (let i = 0; i < history.length; i++) {
-            var divsec = document.createElement('p');
-            divsec.textContent = history[i];
-            WikiData.appendChild(divsec);
-        }
-    }
+      for (let i = 0; i < history.length; i++) {
+          var divsec = document.createElement('p');
+          divsec.textContent = history[i];
+          WikiData.appendChild(divsec);
+      }
+  }
 });
+
